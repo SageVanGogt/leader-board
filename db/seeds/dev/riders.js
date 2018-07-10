e80890989df5d7dd61a781270ec7dc30b32a8663
Eric Mellow <ericmellow@gmail.com>
2018-07-10 15:26:51 -0600

    Add rider data

--- db/seeds/dev/riders.js
+++ db/seeds/dev/riders.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('riders').insert([
-        {id: 1, colName: 'rowValue1'},
+        {id: 1, name: 'rowValue1', gender: '', img: '', country: ''},
         {id: 2, colName: 'rowValue2'},
         {id: 3, colName: 'rowValue3'}
       ]);
