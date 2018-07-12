72f13bdae91c6e34b5996a434da06d8b916c88d5
Sage Vogt <thurmanvogt@gmail.com>
2018-07-11 19:55:58 -0600

    Add before and after each for the api route tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -16,6 +16,27 @@ describe('CLIENT routes', () => {
 });
 
 describe('API routes', () => {
+  
+  beforeEach(function (done) {
+    knex.migrate.rollback()
+      .then(function () {
+        knex.migrate.latest()
+          .then(function () {
+            return knex.seed.run()
+              .then(function () {
+                done();
+              });
+          });
+      });
+  });
+
+  afterEach(function (done) {
+    knex.migrate.rollback()
+      .then(function () {
+        done();
+      });
+  });
+  
   it('should', () => {
 
   });
