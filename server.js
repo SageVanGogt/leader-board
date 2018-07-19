5e0732071cb3c12908579d43cc652ef1efea073b
Eric Mellow <ericmellow@gmail.com>
2018-07-19 16:07:26 -0600

    Change access url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://leaderboard-CO.surge.sh");
+  res.header("Access-Control-Allow-Origin", "http://leaderboard-co.surge.sh");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
